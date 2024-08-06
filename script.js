c0b1e024920e9dc13f74b252a8fca00c69a9ed12
Adhviklal Thoppe <adhviklal.thoppe@gmail.com>
2024-08-06 16:33:41 -0400

    Improved UI and added intro paragraph

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ if(localStorage.getItem("atporttheme") === null) {
 else if(localStorage.getItem("atporttheme") === "light") {
     root.style.setProperty("--primary-color", "#e1e1e1");
     root.style.setProperty("--secondary-color", "#d7d7d7");
-    root.style.setProperty("--tertiary-color", "#c1c1c1");
+    root.style.setProperty("--tertiary-color", "#d0d0d0");
     root.style.setProperty("--accent-color", "#ff3c3c");
+    root.style.setProperty("--secondary-accent-color", "#a70000");
     root.style.setProperty("--text-color", "#0f0f0f");
 
     document.querySelector("#theme-toggle span").innerHTML = "dark_mode";
@@ -16,8 +17,9 @@ else if(localStorage.getItem("atporttheme") === "light") {
 else if(localStorage.getItem("atporttheme") === "dark") {
     root.style.setProperty("--primary-color", "#0f0f0f");
     root.style.setProperty("--secondary-color", "#1a1a1a");
-    root.style.setProperty("--tertiary-color", "#333333");
+    root.style.setProperty("--tertiary-color", "#262626");
     root.style.setProperty("--accent-color", "#ff3c3c");
+    root.style.setProperty("--secondary-accent-color", "#a70000");
     root.style.setProperty("--text-color", "#e1e1e1");
     document.querySelector("#theme-toggle span").innerHTML = "light_mode";
 }
@@ -26,8 +28,9 @@ document.getElementById("theme-toggle").addEventListener('click', () => {
     if(localStorage.getItem("atporttheme") === "dark") {
         root.style.setProperty("--primary-color", "#e1e1e1");
         root.style.setProperty("--secondary-color", "#d7d7d7");
-        root.style.setProperty("--tertiary-color", "#c1c1c1");
+        root.style.setProperty("--tertiary-color", "#d0d0d0");
         root.style.setProperty("--accent-color", "#ff3c3c");
+        root.style.setProperty("--secondary-accent-color", "#a70000");
         root.style.setProperty("--text-color", "#0f0f0f");
         
         localStorage.setItem("atporttheme", "light");
@@ -36,8 +39,9 @@ document.getElementById("theme-toggle").addEventListener('click', () => {
     else if(localStorage.getItem("atporttheme") === "light") {
         root.style.setProperty("--primary-color", "#0f0f0f");
         root.style.setProperty("--secondary-color", "#1a1a1a");
-        root.style.setProperty("--tertiary-color", "#333333");
+        root.style.setProperty("--tertiary-color", "#262626");
         root.style.setProperty("--accent-color", "#ff3c3c");
+        root.style.setProperty("--secondary-accent-color", "#a70000");
         root.style.setProperty("--text-color", "#e1e1e1");
 
         localStorage.setItem("atporttheme", "dark");
