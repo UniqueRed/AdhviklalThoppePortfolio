ec5338cad56a88cae0c687a0403f67072a02bede
Adhviklal Thoppe <adhviklal.thoppe@gmail.com>
2024-08-12 23:11:06 -0400

    Fixed UI

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
 const root = document.documentElement;
 
+function setLight() {
+    root.style.setProperty("--primary-color", "#e1e1e1");
+    root.style.setProperty("--secondary-color", "#d7d7d7");
+    root.style.setProperty("--tertiary-color", "#d0d0d0");
+    root.style.setProperty("--text-color", "#0f0f0f");
+}
+
+function setDark() {
+    root.style.setProperty("--primary-color", "#0f0f0f");
+    root.style.setProperty("--secondary-color", "#1a1a1a");
+    root.style.setProperty("--tertiary-color", "#262626");
+    root.style.setProperty("--text-color", "#e1e1e1");
+}
+
 if(localStorage.getItem("atporttheme") === null) {
+    setDark();
+
     localStorage.setItem("atporttheme", "dark");
 }
 
 else if(localStorage.getItem("atporttheme") === "light") {
-    root.style.setProperty("--primary-color", "#e1e1e1");
-    root.style.setProperty("--secondary-color", "#d7d7d7");
-    root.style.setProperty("--tertiary-color", "#d0d0d0");
-    root.style.setProperty("--accent-color", "#ff3c3c");
-    root.style.setProperty("--secondary-accent-color", "#a70000");
-    root.style.setProperty("--text-color", "#0f0f0f");
+    setLight();
 
     document.querySelector("#theme-toggle span").innerHTML = "dark_mode";
 }
 else if(localStorage.getItem("atporttheme") === "dark") {
-    root.style.setProperty("--primary-color", "#0f0f0f");
-    root.style.setProperty("--secondary-color", "#1a1a1a");
-    root.style.setProperty("--tertiary-color", "#262626");
-    root.style.setProperty("--accent-color", "#ff3c3c");
-    root.style.setProperty("--secondary-accent-color", "#a70000");
-    root.style.setProperty("--text-color", "#e1e1e1");
+    setDark();
+
     document.querySelector("#theme-toggle span").innerHTML = "light_mode";
 }
 
 document.getElementById("theme-toggle").addEventListener('click', () => {
     if(localStorage.getItem("atporttheme") === "dark") {
-        root.style.setProperty("--primary-color", "#e1e1e1");
-        root.style.setProperty("--secondary-color", "#d7d7d7");
-        root.style.setProperty("--tertiary-color", "#d0d0d0");
-        root.style.setProperty("--accent-color", "#ff3c3c");
-        root.style.setProperty("--secondary-accent-color", "#a70000");
-        root.style.setProperty("--text-color", "#0f0f0f");
+        setLight();
         
         localStorage.setItem("atporttheme", "light");
         document.querySelector("#theme-toggle span").innerHTML = "dark_mode";
     }
     else if(localStorage.getItem("atporttheme") === "light") {
-        root.style.setProperty("--primary-color", "#0f0f0f");
-        root.style.setProperty("--secondary-color", "#1a1a1a");
-        root.style.setProperty("--tertiary-color", "#262626");
-        root.style.setProperty("--accent-color", "#ff3c3c");
-        root.style.setProperty("--secondary-accent-color", "#a70000");
-        root.style.setProperty("--text-color", "#e1e1e1");
+        setDark();
 
         localStorage.setItem("atporttheme", "dark");
         document.querySelector("#theme-toggle span").innerHTML = "light_mode";
